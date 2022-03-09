c402e5ed6bd83d282f9b6df1ef04cbb64d432638
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-09 16:17:42 +0100

    dice game first try

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-// const random = [];
-
-// for (let i = 0; i < 10; i++) {
-//     const number = Math.floor((Math.random() * 10) + 1);
-//     random.push(number);
-//     console.log(random);
-// }
-
-// console.log(random);
-
-
-// // dato un array con dei nomi li vogliamo stampare tutti su una riga nel console.log
-
-// const studenti = ["Luigi", "Mario", "Lucio"];
-
-// let studentiSullaRiga = "";
-
-// for (let i = 0; i < studenti.length; i++) {
-//     studentiSullaRiga = studentiSullaRiga + studenti[i];
-// }
-
-// console.log(studentiSullaRiga);
-
 
 
 // Mail
 // Chiedi all’utente la sua email,
 //     controlla che sia nella lista di chi può accedere,
 //         stampa un messaggio appropriato sull’esito del controllo.
-// Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte ?
-//     Se dobbiamo confrontare qualcosa che "cosa" ci serve ?
+
 
 //declare array valid emails
 const admins = ['pippo@gmail.com', 'paperino@yahoo.net', 'pluto@libero.it'];
@@ -50,3 +22,20 @@ for (let i = 0; i < admins.length; i++) {
 
 //log proper message, one time, outside of loop - standard message is invalid if email is not matching
 console.log(isValid);
+
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte ?
+//     Se dobbiamo confrontare qualcosa che "cosa" ci serve ?
+
+
+let playerDice = Math.floor(Math.random() * 6) + 1;
+let computerDice = Math.floor(Math.random() * 6) + 1;
+console.log(playerDice);
+console.log(computerDice);
+
+console.log(Math.max(playerDice, computerDice)); 
